a96bd9e6b308df90779098f974aee46418a7538e
wilglison <wilgilson.niky@gmail.com>
2024-06-07 18:49:16 -0300

    Primeira aula

---
+++ Aula1/src/main/java/br/ufg/inf/backend/HelloServlet.java
@@ -0,0 +1,17 @@
+package br.ufg.inf.backend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse
+			response) throws ServletException, IOException {
+		response.getWriter().println( "Hello, World!" );
+	}
+}
