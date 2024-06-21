315dfb7c533a8fd7edade4bae1a019926e49f999
wilglison <wilgilson.niky@gmail.com>
2024-06-21 19:38:56 -0300

    Novos projetos

---
+++ projeto-spring/src/test/java/br/ufg/inf/backend/spring/ProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.backend.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
